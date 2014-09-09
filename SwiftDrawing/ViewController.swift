c578eed79d7bbd80474d9278edb04ba3badc00d2
adamskyle <kpe.adams@icloud.com>
2014-09-09 11:25:57 +0200

    Initial Commit

---
+++ SwiftDrawing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftDrawing
+//
+//  Created by Kyle Adams on 09/09/14.
+//  Copyright (c) 2014 Kyle Adams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
